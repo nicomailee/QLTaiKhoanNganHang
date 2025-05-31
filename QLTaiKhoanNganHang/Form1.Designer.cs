527bf8f3996405e5e601b6cf60c80d20e1b62016
nicomailee <maileltm163@gmail.com>
2025-05-31 22:56:04 +0700

    có thay đổi

---
+++ QLTaiKhoanNganHang/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace QLTaiKhoanNganHang
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panelLeft = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBoxBank = new System.Windows.Forms.PictureBox();
+            this.panelRight = new System.Windows.Forms.Panel();
+            this.linkTerms = new System.Windows.Forms.LinkLabel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panelLogin = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_Thoat = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btn_Dangnhap = new System.Windows.Forms.Button();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.chk_ShowPassword = new System.Windows.Forms.CheckBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.panelLeft.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBank)).BeginInit();
+            this.panelRight.SuspendLayout();
+            this.panelLogin.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelLeft
+            // 
+            this.panelLeft.BackColor = System.Drawing.Color.LightBlue;
+            this.panelLeft.Controls.Add(this.label1);
+            this.panelLeft.Controls.Add(this.pictureBoxBank);
+            this.panelLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelLeft.Location = new System.Drawing.Point(0, 0);
+            this.panelLeft.Name = "panelLeft";
+            this.panelLeft.Size = new System.Drawing.Size(434, 868);
+            this.panelLeft.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(116, 402);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 28);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "NGÂN HÀNG 3CE";
+            // 
+            // pictureBoxBank
+            // 
+            this.pictureBoxBank.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxBank.Image")));
+            this.pictureBoxBank.Location = new System.Drawing.Point(54, 179);
+            this.pictureBoxBank.Name = "pictureBoxBank";
+            this.pictureBoxBank.Size = new System.Drawing.Size(297, 209);
+            this.pictureBoxBank.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxBank.TabIndex = 0;
+            this.pictureBoxBank.TabStop = false;
+            // 
+            // panelRight
+            // 
+            this.panelRight.BackColor = System.Drawing.Color.LightBlue;
+            this.panelRight.Controls.Add(this.linkTerms);
+            this.panelRight.Controls.Add(this.label3);
+            this.panelRight.Controls.Add(this.label2);
+            this.panelRight.Controls.Add(this.panelLogin);
+            this.panelRight.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelRight.Location = new System.Drawing.Point(434, 0);
+            this.panelRight.Name = "panelRight";
+            this.panelRight.Size = new System.Drawing.Size(979, 868);
+            this.panelRight.TabIndex = 1;
+            // 
+            // linkTerms
+            // 
+            this.linkTerms.AutoSize = true;
+            this.linkTerms.Location = new System.Drawing.Point(506, 669);
+            this.linkTerms.Name = "linkTerms";
+            this.linkTerms.Size = new System.Drawing.Size(144, 16);
+            this.linkTerms.TabIndex = 9;
+            this.linkTerms.TabStop = true;
+            this.linkTerms.Text = "Điều khoản và bảo mật";
+            this.linkTerms.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkTerms_LinkClicked);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label3.Location = new System.Drawing.Point(366, 291);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(305, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "QUẢN LÍ TÀI KHOẢN NGÂN HÀNG";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label2.Location = new System.Drawing.Point(369, 273);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Project";
+            // 
+            // panelLogin
+            // 
+            this.panelLogin.Controls.Add(this.label4);
+            this.panelLogin.Controls.Add(this.btn_Thoat);
+            this.panelLogin.Controls.Add(this.label5);
+            this.panelLogin.Controls.Add(this.btn_Dangnhap);
+            this.panelLogin.Controls.Add(this.txtUsername);
+            this.panelLogin.Controls.Add(this.chk_ShowPassword);
+            this.panelLogin.Controls.Add(this.txtPassword);
+            this.panelLogin.Location = new System.Drawing.Point(372, 325);
+            this.panelLogin.Name = "panelLogin";
+            this.panelLogin.Size = new System.Drawing.Size(345, 172);
+            this.panelLogin.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 16);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Tên đăng nhập";
+            // 
+            // btn_Thoat
+            // 
+            this.btn_Thoat.Location = new System.Drawing.Point(213, 128);
+            this.btn_Thoat.Name = "btn_Thoat";
+            this.btn_Thoat.Size = new System.Drawing.Size(75, 23);
+            this.btn_Thoat.TabIndex = 8;
+            this.btn_Thoat.Text = "Thoát";
+            this.btn_Thoat.UseVisualStyleBackColor = true;
+            this.btn_Thoat.Click += new System.EventHandler(this.btn_Thoat_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(33, 60);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 16);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Nhập mật khẩu";
+            // 
+            // btn_Dangnhap
+            // 
+            this.btn_Dangnhap.Location = new System.Drawing.Point(82, 128);
+            this.btn_Dangnhap.Name = "btn_Dangnhap";
+            this.btn_Dangnhap.Size = new System.Drawing.Size(111, 23);
+            this.btn_Dangnhap.TabIndex = 7;
+            this.btn_Dangnhap.Text = "Đăng nhập";
+            this.btn_Dangnhap.UseVisualStyleBackColor = true;
+            this.btn_Dangnhap.Click += new System.EventHandler(this.btn_Dangnhap_Click);
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(137, 21);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(151, 22);
+            this.txtUsername.TabIndex = 4;
+            // 
+            // chk_ShowPassword
+            // 
+            this.chk_ShowPassword.AutoSize = true;
+            this.chk_ShowPassword.Location = new System.Drawing.Point(137, 85);
+            this.chk_ShowPassword.Name = "chk_ShowPassword";
+            this.chk_ShowPassword.Size = new System.Drawing.Size(114, 20);
+            this.chk_ShowPassword.TabIndex = 6;
+            this.chk_ShowPassword.Text = "Hiện mật khẩu";
+            this.chk_ShowPassword.UseVisualStyleBackColor = true;
+            this.chk_ShowPassword.CheckedChanged += new System.EventHandler(this.chk_ShowPassword_CheckedChanged);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(137, 57);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(151, 22);
+            this.txtPassword.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(1413, 868);
+            this.Controls.Add(this.panelRight);
+            this.Controls.Add(this.panelLeft);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Đăng nhập";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panelLeft.ResumeLayout(false);
+            this.panelLeft.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBank)).EndInit();
+            this.panelRight.ResumeLayout(false);
+            this.panelRight.PerformLayout();
+            this.panelLogin.ResumeLayout(false);
+            this.panelLogin.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelLeft;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBoxBank;
+        private System.Windows.Forms.Panel panelRight;
+        private System.Windows.Forms.Button btn_Thoat;
+        private System.Windows.Forms.Button btn_Dangnhap;
+        private System.Windows.Forms.CheckBox chk_ShowPassword;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel linkTerms;
+        private System.Windows.Forms.Panel panelLogin;
+    }
+}
+
---
+++ QLTaiKhoanNganHang/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLTaiKhoanNganHang
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Resize += Form1_Resize;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Mở full màn hình khi chạy
+            this.WindowState = FormWindowState.Maximized;
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+            // Màu nền đồng nhất
+            this.BackColor = Color.WhiteSmoke;
+            panelRight.BackColor = this.BackColor;
+
+            // Căn giữa panel đăng nhập
+            CenterLoginPanel();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            CenterLoginPanel();
+        }
+
+        private void CenterLoginPanel()
+        {
+            panelLogin.Left = (panelRight.ClientSize.Width - panelLogin.Width) / 2;
+            panelLogin.Top = (panelRight.ClientSize.Height - panelLogin.Height) / 2;
+        }
+
+        private void chk_ShowPassword_CheckedChanged(object sender, EventArgs e)
+        {
+            txtPassword.UseSystemPasswordChar = !chk_ShowPassword.Checked;
+        }
+
+        private void btn_Thoat_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btn_Dangnhap_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            if (username == "Lemai" && password == "123")
+            {
+                MessageBox.Show("Đăng nhập thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Sai tên đăng nhập hoặc mật khẩu!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void linkTerms_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            MessageBox.Show("Chính sách và điều khoản đang được cập nhật.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
